63efa857a975fafa9035a557e5b3b91e2723fbb9
Max Mumford <max@rok.digital>
2020-05-02 15:02:43 +0100

    Add news comp and shared project

---
+++ libs/shared/src/lib/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsComponent } from './news/news.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [NewsComponent],
+  exports: [NewsComponent],
+})
+export class SharedModule {}
